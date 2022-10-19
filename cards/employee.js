8d9652db293032277521d3f7f90bbb1afe5942f0
CoderGirl <angi.adema@gmail.com>
2022-10-19 17:16:27 -0600

    added employee info code to employee.js

--- cards/employee.js
+++ cards/employee.js
@@ -1,1 +1,28 @@
-//Write function to return name, ID, email and role.
+//Variable to import inquirer???
+//Write constructor function to return name, ID, email and role.
+class Employee {
+    constructor (name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email
+    }
+    //Return input name.
+    getName () {
+        return this.name;
+    }
+    //Return input id.
+    getID () {
+        return this.id;
+    }
+    //Return input email.
+    getEmail () {
+        return this.email;
+    }
+    //Return input role.
+    getRole () {
+        return 'Employee';
+    }
+}
+//Export file.
+module.exports = Employee;
+
--- cards/engineer.js
+++ cards/engineer.js
@@ -1,1 +1,4 @@
+//Variable to import inquirer???
+const Employee = require('./employee');
+
 //Write function to return name, ID, email and role.
