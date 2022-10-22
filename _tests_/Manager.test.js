811bcc5f9d9db8bbe000ab29bf753694afdf31bd
CoderGirl <angi.adema@gmail.com>
2022-10-22 13:35:33 -0600

    updated manager.test.js file

--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -1,1 +1,18 @@
 //Write the test for this class.
+const Manager = require('../lib/Manager');
+
+require('manager', () => {
+    describe('generate manager object', () => {
+        it('should generate a manager object from data input', () => {
+            const manager = new Manager('Shelley', 26, 'shelley@gmail.com', 2);
+
+            expect(manager.officeNumber).toEqual(expect.any(Number));
+        });
+    });
+
+    describe('get employee role', () => {
+        const manager = new Manager('Shelley', 26, 'shelley@gmail.com', 2);
+
+        expect(manager.getRole()).toEqual('Manager');
+    });
+});
