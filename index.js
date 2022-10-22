e533129cd175034ffa1c133c6e13a46bdf9b4968
CoderGirl <angi.adema@gmail.com>
2022-10-22 11:40:09 -0600

    added code to index.js file

--- index.js
+++ index.js
@@ -8,29 +8,36 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
+const staffArray = [];
+
 function managerQuestions() {
     inquirer.prompt([
         {
             name: "name",
             type: "input",
-            message: ""
+            message: "Who is the team manager?"
         },
         {
             name: "id",
             type: "input",
-            message: ""
+            message: "What is the manager's id number?"
         },
         {
             name: "email",
             type: "input",
-            message: ""
+            message: "What is the manager's email address?"
         },
         {
             name: "officeNumber",
             type: "input",
-            message: ""
+            message: "What is the manager's office number?"
         },
     ]).then((answers) => {
+        const { name, id, email, officeNumber } = answers;
+        const manager = new Manager (name, id, email, officeNumber);
+
+        staffArray.push(manager);
+        console.log(manager);
 
     }).then(() => menu())
 }
@@ -40,24 +47,29 @@ function engineerQuestions() {
         {
             name: "name",
             type: "input",
-            message: ""
+            message: "What is the engineer's name?"
         },
         {
             name: "id",
             type: "input",
-            message: ""
+            message: "What is the engineer's id?"
         },
         {
             name: "email",
             type: "input",
-            message: ""
+            message: "What is the engineer's email address?"
         },
         {
             name: "github",
             type: "input",
-            message: ""
+            message: "What is the engineer's GitHub username?"
         },
     ]).then((answers) => {
+        const { name, id, email, github } = answers;
+        const engineer = new Engineer (name, id, email, github);
+
+        staffArray.push(engineer);
+        console.log(engineer);
 
     }).then(() => menu())
 }
@@ -67,24 +79,29 @@ function internQuestions() {
         {
             name: "name",
             type: "input",
-            message: ""
+            message: "What is the name of the intern?"
         },
         {
             name: "id",
             type: "input",
-            message: ""
+            message: "What is the intern's id number?"
         },
         {
             name: "email",
             type: "input",
-            message: ""
+            message: "What is the intern's email address?"
         },
         {
             name: "school",
             type: "input",
-            message: ""
+            message: "What is the name of the intern's school?"
         },
     ]).then((answers) => {
+        const { name, id, email, school } = answers;
+        const intern = new Intern (name, id, email, school);
+
+        staffArray.push(intern);
+        console.log(intern);
 
     }).then(() => menu())
 }
@@ -112,12 +129,58 @@ function menu() {
     })
 }
 
+//Saw this and am trying to repurpose it here.
+// .then(employeeData => {
+//     let { name, id, email, role, github, school, confirmAddEmployee } = employeeData;
+//     let employee;
+
+//     if (role === 'Engineer') {
+//         employee = new Engineer (name, id, email, github);
+
+//         console.log(employee);
+
+//     } else if (role === 'Intern') {
+//         employee = new Intern (name, id, email, school);
+
+//         console.log(employee);
+//     }
+//     staffArray.push(employee);
+
+//     if(confirmAddEmployee) {
+//         return confirmAddEmployee(teamArray);
+//     } else {
+//         return staffArray;
+//     }
+// })
+
+const addEmployee = () => {
+    console.log("Adding employees to the team");
+}
+
+
 function buildHtml() {
-    fs.writeFileSync('./dist/index.html', generateHTML())
+    fs.writeFileSync('./dist/index.html', generateHTML(), err => {
+        if (err) {
+            console.log(err);
+            return;
+        } else {
+            console.log("Staff profile successfully created!")
+        }
+    })
 }
 
 function start() {
     managerQuestions()
+        .then(addEmployee)
+        .then(staffArray => {
+            return generateHTML(staffArray);
+        })
+        .then(pageHTML => {
+            return writeFile(pageHTML);
+        })
+        .catch(err => {
+            console.log(err);
+        })
 }
 
 start()
