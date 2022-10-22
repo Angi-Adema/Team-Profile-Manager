d0afc6c2086c02fe5fbd20ba19a135fd27436519
CoderGirl <angi.adema@gmail.com>
2022-10-22 13:25:57 -0600

    updated code in intern.test.js file.

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -9,30 +9,30 @@ describe('employee', () => {
             expect(employee.name).toEqual(expect.any(String));
             expect(employee.id).toEqual(expect.any(Number));
             expect(employee.email).toEqual(expect.any(String));
-        })
-    })
+        });
+    });
 
     describe('get employee name', () => {
         const employee = new Employee('Shmo', 20, 'shmo2@yahoo.com');
 
         expect(employee.getName()).toEqual(expect.any(String));
-    })
+    });
 
     describe('get employee id', () => {
         const employee = new Employee('Shmo', 20, 'shmo2@yahoo.com');
 
         expect(employee.getID()).toEqual(expect.any(Number));
-    })
+    });
 
     describe('get employee email', () => {
         const employee = new Employee('Shmo', 20, 'shmo2@yahoo.com');
 
         expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
-    })
+    });
 
     describe('get employee role', () => {
         const employee = new Employee('Shmo', 20, 'shmo2@yahoo.com');
 
         expect(employee.getRole()).toEqual('Employee');
-    })
-})
+    });
+});
--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -1,1 +1,24 @@
 //Write the test for this class.
+const Intern = require('../lib/Intern');
+
+describe('intern', () => {
+    describe('generate intern object', () => {
+        it('should generate an intern object from data input', () => {
+            const intern = new Intern('Dan', 24, 'dan@gmail.com', 'DU');
+
+            expect(intern.school).toEqual(expect.any(String));
+        });
+    });
+
+    describe('get name of school', () => {
+        const intern = new Intern('Dan', 24, 'dan@gmail.com', 'DU');
+
+        expect(intern.getSchool()).toEqual(expect.stringContaining(intern.school.toString()));
+    });
+
+    describe('get employee role', () => {
+        const intern = new Intern('Dan', 24, 'dan@gmail.com', 'DU');
+
+        expect(intern.getRole()).toEqual('Intern');
+    });
+});
